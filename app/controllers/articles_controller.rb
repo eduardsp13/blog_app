f3715c9ab70dd90ee150520a6f58a95f0703c80e
eduardsp13 <eduardsp13@gmail.com>
2019-09-27 02:02:18 -0300

    add the destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
     render 'edit'
     end  
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to article_path    
+  end
   
 
   
